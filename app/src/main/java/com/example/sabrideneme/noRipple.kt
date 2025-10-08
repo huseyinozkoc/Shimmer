4282949fc15f4822537e3839b3d3ae0a97761ab9
Hüseyin Özkoç <huseyin.ozko225@gmail.com>
2025-10-08 11:54:42 +0300

    ..as

---
+++ app/src/main/java/com/example/sabrideneme/noRipple.kt
@@ -0,0 +1,7 @@
+"**/*\$Compose^{*}", // Compose tarafından üretilen sentetik sınıflar
+"**/*\$lambda\$*",   // Lambda'lar için üretilen sınıflar
+"**/*\$internal\$*", // Compose internal sınıflar
+"**/*\$fast*", // Compose compiler optimize edilmiş kodları
+"**/*\$TypeChecking*", // Compose Type Checking generated
+"**/*Kt\$*", // Bazen top-level fonksiyonlar için oluşan sınıflar
+"**/*ComposableSingletons*", // Bazı singleton composable'lar
